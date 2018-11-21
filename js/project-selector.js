e05cf8f4fed803f7b09cf6921cd701b7839e9bcc
Connor Vernon <conjamver@hotmail.com>
2018-11-21 17:30:50 +1100

    JS feature - Hide previous opened project when view current project

--- js/project-selector.js
+++ js/project-selector.js
@@ -5,15 +5,27 @@ var placementSelector = $('#placementCard');
 var placementArticle = $('#article-placement');
 var personalSelector = $('#personalCard');
 
-
     //show or hide the article - CAPSTONE
     capstoneSelector.click(function(){
-        capstoneArticle.slideToggle(500);
+        if(placementArticle.css("display") != "none"){
+            placementArticle.css("display","none");
+        }
+         //toggle the slide function and then scroll to position
+        capstoneArticle.slideToggle(750);
+         $("html, body").animate({ scrollTop: $("#article-capstone").offset().top }, 600);
     });
+    
     //show or hide the article - PLACEMENT
     placementSelector.click(function(){
-        placementArticle.slideToggle(500);
+        //hide an open article
+        if(capstoneArticle.css("display") != "none"){
+            capstoneArticle.css("display","none");
+        }
+        //toggle the slide function and then scroll to position
+        placementArticle.slideToggle(750);
+        $("html, body").animate({ scrollTop: $("#article-placement").offset().top }, 600);
     });
+    
     //show or hide the article - PLACEMENT
     personalSelector.click(function(){
         alert("Personal projects will be showcased to the portfolio soon. If you would like to see progress, visit: https://github.com/conjamver ");
